6dbe9e7067459af0fb263a4cfc60a6caa906de10
Miguel De La Cruz <iscrusman@gmail.com>
2018-10-25 15:37:58 -0700

    Access-Control-Allow-Origin
    Access-Control-Allow-Origin

--- app.js
+++ app.js
@@ -12,6 +12,13 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+  
 //Servicio de blockchain
 app.use("/api", routes);
 
